f7a019c7af3908ef26e34050d3c791dc7656e8a6
Ysa <isabelle.yim@gmail.com>
2022-02-02 18:46:46 +0100

    feat: write query to retrieve freelancer's profile details

--- api/src/schema.ts
+++ api/src/schema.ts
@@ -1,24 +1,6 @@
 import { gql } from 'graphql-tag'
-import { Language, Resolvers } from '../generated/graphql'
-
-/**
- * Use this object mock as if it was retrieved from a database.
- */
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const MOCK_FREELANCER = {
-  id: 'a6af7583-ff4b-44b5-abc7-daf5444f3b9b',
-  avatar: {
-    '64x64': '/avatar/a6af7583-ff4b-44b5-abc7-daf5444f3b9b/64x64.png',
-    '256x256': '/avatar/a6af7583-ff4b-44b5-abc7-daf5444f3b9b/256x256.png',
-    '512x512': '/avatar/a6af7583-ff4b-44b5-abc7-daf5444f3b9b/512x512.png',
-  },
-  firstname: 'Gordon',
-  lastname: 'Champlin',
-  language: Language.KLINGON,
-  birthDate: new Date(1990, 12, 31).toISOString(),
-  isVisible: false,
-  retribution: 500,
-}
+import { Resolvers } from '../generated/graphql'
+import { MOCK_FREELANCER } from './db'
 
 export const typeDefs = gql`
   type Avatar {
@@ -33,13 +15,21 @@ export const typeDefs = gql`
     KLINGON
   }
 
-  # Todo: Write a type for the freelancer's profile.
+  # Type for the freelancer's profile
+  type Profile {
+    id: ID!
+    firstname: String!
+    lastname: String!
+    isVisible: Boolean
+    retribution: Int
+    birthDate: String
+    avatar: Avatar
+    language: Language
+  }
 
+  # Retrieve the current freelancer's profile
   type Query {
-    helloWorld: String!
-
-    # Todo: Write a query to retrieve the current freelancer's profile.
-    # Ex: myProfile: Profile!
+    myProfile: Profile!
   }
 
   # Todo: Write a mutation to update the current freelancer's profile.
@@ -48,20 +38,29 @@ export const typeDefs = gql`
   # }
 `
 
+function formatAvatar(avatar?: typeof MOCK_FREELANCER.avatar) {
+  if (!avatar) return null
+  return {
+    smallUrl: avatar['64x64'],
+    largeUrl: avatar['256x256'],
+    xLargeUrl: avatar['512x512'],
+  }
+}
+
 export const resolvers: Resolvers = {
   Query: {
-    /**
-     * Example of resolver.
-     */
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    helloWorld(root, args, context) {
-      return 'Hello!'
-    },
-
     /**
      * @Todo
      * Implement the query to retrieve the current freelancer's profile.
      */
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    myProfile(root, args, context) {
+      const avatar = formatAvatar(MOCK_FREELANCER.avatar)
+      return {
+        ...MOCK_FREELANCER,
+        avatar,
+      }
+    },
   },
 
   /**
