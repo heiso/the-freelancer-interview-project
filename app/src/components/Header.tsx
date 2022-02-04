02f24adcd8fe53b06ba36c98d5f30497a1ba285e
Ysa <isabelle.yim@gmail.com>
2022-02-04 11:39:47 +0100

    feat: add link to home page from header

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,9 +1,11 @@
-const Header = () => {
+import { Link } from 'react-router-dom'
+
+function Header() {
   return (
     <nav className="bg-gray-200 p-6 mb-6">
-      <div>
-        <span className="font-semibold text-xl tracking-tight">Comet</span>
-      </div>
+      <h1 className="font-semibold text-xl tracking-tight">
+        <Link to={'/'}>Comet</Link>
+      </h1>
     </nav>
   )
 }
