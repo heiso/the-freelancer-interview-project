c306f76638191c78a7bd4db314d2947dfc5a7b4d
Ysa <isabelle.yim@gmail.com>
2022-02-04 11:40:11 +0100

    fix: fix layout

--- app/src/components/Footer.tsx
+++
@@ -1,5 +0,0 @@
-const Footer = () => {
-  return <footer className="text-xs text-center mt-auto py-8">Isabelle Yim - 2022</footer>
-}
-
-export default Footer
--- app/src/components/Layout.tsx
+++ app/src/components/Layout.tsx
@@ -1,13 +1,16 @@
-import Footer from './Footer'
 import Header from './Header'
 
-const Layout = ({ children }) => {
+type LayoutProps = {
+  children: React.ReactNode
+}
+
+function Layout({ children }: LayoutProps) {
   return (
-    <>
+    <div>
       <Header />
       <main>{children}</main>
-      <Footer />
-    </>
+    </div>
   )
 }
+
 export default Layout
